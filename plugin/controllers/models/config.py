oe-alliance/OpenWebif

* remove duplicate brackets * remove redundant open flag

plugin/controllers/models/config.py
@@ -338,9 +338,9 @@ class ConfigFiles:
 		locations = ('SystemPlugins', 'Extensions')
 		libdir = eEnv.resolve('${libdir}')
 		for location in locations:
-			plugins = listdir(('%s/enigma2/python/Plugins/%s' % (libdir, location)))
+			plugins = listdir('%s/enigma2/python/Plugins/%s' % (libdir, location))
 			for plugin in plugins:
-				setupfiles.append(('%s/enigma2/python/Plugins/%s/%s/setup.xml' % (libdir, location, plugin)))
+				setupfiles.append('%s/enigma2/python/Plugins/%s/%s/setup.xml' % (libdir, location, plugin))
 		for setupfile in setupfiles:
 			if exists(setupfile):
 				plugindir = dirname(setupfile)
@@ -400,7 +400,7 @@ class ConfigFiles:
 		sections = []
 		for setupfileName, pluginLanguageDomain in self.setupfiles:
 			# print("[OpenWebif] loading configuration file : %s" % setupfile)
-			setupfile = open(setupfileName, 'r')
+			setupfile = open(setupfileName)
 			setupdom = parse(setupfile)  # nosec
 			setupfile.close()
 			xmldata = setupdom.getroot()

plugin/controllers/models/movies.py
@@ -439,7 +439,7 @@ def movieActionService(serviceref, dest, name, domove, newname, action):
 						break
 				if metafilename and exists(metafilename):
 					lines = []
-					with open(metafilename, 'r') as fd:
+					with open(metafilename) as fd:
 						lines = fd.read().splitlines()
 					lines[1] = newname
 					with open(metafilename, 'w') as fd:
@@ -470,7 +470,7 @@ def getMovieInfo(sref=None, addtag=None, deltag=None, title=None, cuts=None, des
 			metafilename = pathjoin(srcPath, "%s.meta" % srcName)
 			if exists(metafilename):
 				lines = []
-				with open(metafilename, 'r') as fd:
+				with open(metafilename) as fd:
 					lines = fd.read().splitlines()
 				if lines:
 					meta = ["", "", "", "", "", "", ""]

kueken/e2openplugin-OpenWebif

plugin/controllers/models/config.py
@@ -309,9 +309,9 @@ class ConfigFiles:
 		locations = ('SystemPlugins', 'Extensions')
 		libdir = eEnv.resolve('${libdir}')
 		for location in locations:
-			plugins = listdir(('%s/enigma2/python/Plugins/%s' % (libdir, location)))
+			plugins = listdir('%s/enigma2/python/Plugins/%s' % (libdir, location))
 			for plugin in plugins:
-				setupfiles.append(('%s/enigma2/python/Plugins/%s/%s/setup.xml' % (libdir, location, plugin)))
+				setupfiles.append('%s/enigma2/python/Plugins/%s/%s/setup.xml' % (libdir, location, plugin))
 		for setupfile in setupfiles:
 			if path.exists(setupfile):
 				self.setupfiles.append(setupfile)
@@ -367,7 +367,7 @@ class ConfigFiles:
 		sections = []
 		for setupfile in self.setupfiles:
 			# print("[OpenWebif] loading configuration file :", setupfile)
-			setupfile = open(setupfile, 'r')
+			setupfile = open(setupfile)
 			setupdom = xml.etree.cElementTree.parse(setupfile)  # nosec
 			setupfile.close()
 			xmldata = setupdom.getroot()

plugin/controllers/models/movies.py
@@ -658,7 +658,7 @@ def getMovieInfo(sRef=None, addtag=None, deltag=None, title=None, cuts=None, des
 			metafilename = '/' + filename + '.meta'
 			if fileExists(metafilename):
 				lines = []
-				with open(metafilename, 'r') as f:
+				with open(metafilename) as f:
 					lines = f.readlines()
 				if lines:
 					meta = ["", "", "", "", "", "", ""]